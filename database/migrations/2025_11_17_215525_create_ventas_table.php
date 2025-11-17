1d3af089bc9d6adcc1a9ac3547628f73431db686
Julio <202300081@uprr.edu.mx>
2025-11-17 15:59:33 -0600

    Migración tabla ventas
    Migración para la tabla de ventas

---
+++ database/migrations/2025_11_17_215525_create_ventas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('fecha')->useCurrent();
+            $table->decimal('total', 10, 2);
+            $table->decimal('impuesto', 10, 2)->default(0);
+            $table->decimal('descuento', 10, 2)->default(0);
+            $table->enum('metodo_pago', ['efectivo', 'tarjeta', 'transferencia'])->default('efectivo'); 
+            $table->enum('estado', ['pendiente', 'completada', 'cancelada'])->default('pendiente');
+            $table->timestamps();
+
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ventas');
+    }
+};
